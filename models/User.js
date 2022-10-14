9e33171d49f34d20304afbb4b8b7cfed21a5d0de
bigsmile0128 <goldwebnike@gmail.com>
2022-10-13 23:44:53 -0700

    fix: fix database field name

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const UserSchema = new Schema(
             type: String,
             required: true,
         },
-        user_customeURL: {
+        user_customURL: {
             type: String,
         },
         user_bio: {
